b27401d3797d91de3d50f4e62f267cc0a0ce9e48
JosephABB <josephbenitez203@gmail.com>
2022-08-28 13:43:20 -0700

    Completed loadDF

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from auth import authenticate
+from loadDF import load
 
 user_in = input('Enter subreddit in following format "r/news": ')
-x = authenticate('WjBdU3M9ao2qn8P1SZ6EmQ', 'C:/Users/josep/wordCount/secretKey.txt', user_in)
+raw_data = authenticate('WjBdU3M9ao2qn8P1SZ6EmQ', 'C:/Users/josep/wordCount/secretKey.txt', user_in)
 
-print(x)
+titles_df = load(raw_data)
+
+print(titles_df.to_string())
